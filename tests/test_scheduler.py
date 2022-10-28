210c122771516659e24474debaa736aa3141214e
Ryan Sneyd <rts1234567@hotmail.com>
2022-10-28 15:05:38 -0400

    add scheduler automation

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,7 +1,7 @@
 from models.repository import SchedulerRepository
 from models.request import Request
 from models.cloud_run import CloudRun
-from models.graphql_inputs import MaintenanceTicketInput
+from models.graphql_types import MaintenanceTickett, Lease, Tenant
 
 import json, aiohttp
 
@@ -11,7 +11,12 @@ repository = SchedulerRepository(cloudRun.get_scheduler_test_hostname())
 
 async def test_Router_schedules_maintenance_ticket_upload_successfully():
     async with aiohttp.ClientSession() as session:
-        maintenanceTicket = MaintenanceTicketInput(**{
+        maintenanceTicket = MaintenanceTicket(**{
+                "id": 1,
+                "name": "MaintenanceTicket",
+                "imageURL": "str",
+                "datePosted": "str",
+                "firebaseId": "str",
                 "houseId": 3,
                 "description": {
                     "descriptionText": "fdagfdasfsdfasdfa"
@@ -34,13 +39,13 @@ async def test_Router_schedules_generate_lease_upload_successfully():
     async with aiohttp.ClientSession() as session:
         with open(r"./tests/lease_test.json", mode="r") as lease_json:
             leaseData = json.load(lease_json)
-            lease = LeaseInput(**leaseData)
+            lease = Lease(**leaseData)
         monad = await repository.schedule_lease("FirebaseID", lease)
         assert monad.get_param_at(0) == {"status": "Job scheduled successfully"}
 
 async def test_Router_schedules_add_tenant_email_upload_successfully():
     async with aiohttp.ClientSession() as session:
-        tenant = TenantInput(**{
+        tenant = Tenant(**{
             "firstName": "Timmy11",
             "lastName": "Tenant",
             "email": "a@s.com",
@@ -52,7 +57,7 @@ async def test_Router_schedules_add_tenant_email_upload_successfully():
 
 async def test_Router_schedules_sign_tenant_upload_successfully():
     async with aiohttp.ClientSession() as session:
-        tenant = TenantInput(**{
+        tenant = Tenant(**{
             "firstName": "Timmy11",
             "lastName": "Tenant",
             "email": "a@s.com",
