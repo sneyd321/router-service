6ecdf7e0048d51d170cd023e71bd51b0a570c0d0
Ryan Sneyd <rts1234567@hotmail.com>
2022-10-30 12:37:53 -0400

    add outage automation

--- tests/test_timeout.py
+++ tests/test_timeout.py
@@ -1,13 +1,15 @@
-from models.repository import HouseRepsository
+from models.repository import HouseRepository
 from models.request import Request
 from models.cloud_run import CloudRun
 
+import aiohttp
+
+cloudRun = CloudRun()
+cloudRun.discover_dev()
 
 async def test_Router_returns_error_when_fails_to_connect_to_house_service():
-    cloudRun = CloudRun()
-    cloudRun.discover_dev()
-    request = Request(cloudRun.get_house_hostname(), "/House")
-    repository = HouseRepsository(request)
-    monad = await repository.insert_house(landlordId=1)
-    assert monad.error_status == {"status": 408, "reason": "Failed to connect downstream service"}
+    async with aiohttp.ClientSession() as session:
+        repository = HouseRepository(cloudRun.get_house_hostname())
+        monad = await repository.create_house(session, landlordId=1)
+        assert monad.error_status == {"status": 408, "reason": "Failed to connect downstream service"}
 
