400b117d42e02b00597b7a9c510589a6c5d389cd
Ryan Sneyd <rts1234567@hotmail.com>
2022-10-30 12:26:01 -0400

    add repository tests

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,25 +1,29 @@
 from models.repository import Repository
 from models.request import Request
-from models.cloud_run import CloudRun
+import aiohttp
 
+repository = Repository()
 
 async def test_Router_returns_error_when_house_service_insert_returns_non_json_data():
-    request = Request("https://0b008cd5-f83f-40f0-90e8-3c676e55b481.mock.pstmn.io", "/Landlord/1/House")
-    repository = Repository(request)
-    monad = await repository.get()
-    assert monad.error_status == {"status": 502, "reason": "Recieved invalid downstream response"}
+    async with aiohttp.ClientSession() as session:
+        request = Request("https://0b008cd5-f83f-40f0-90e8-3c676e55b481.mock.pstmn.io", "/Landlord/1/House")
+        request.set_session(session)
+        monad = await repository.get(request)
+        assert monad.error_status == {"status": 502, "reason": "Recieved invalid downstream response"}
 
 async def test_Router_returns_error_when_a_500_response_is_returned_on_insert():
-    request = Request("https://0b008cd5-f83f-40f0-90e8-3c676e55b481.mock.pstmn.io", "/House")
-    repository = Repository(request)
-    monad = await repository.insert(landlordId=1)
-    assert monad.error_status == {"status": 500, "reason": "An unexpected error occured"}
+    async with aiohttp.ClientSession() as session:
+        request = Request("https://0b008cd5-f83f-40f0-90e8-3c676e55b481.mock.pstmn.io", "/House")
+        request.set_session(session)
+        monad = await repository.post(request, landlordId=1)
+        assert monad.error_status == {"status": 500, "reason": "An unexpected error occured"}
 
 async def test_Router_returns_error_when_a_422_response_is_returned_on_insert():
-    cloudRun = CloudRun()
-    cloudRun.discover_dev()
-    request = Request(cloudRun.get_house_hostname(), "/House")
-    repository = Repository(request)
-    monad = await repository.insert(landlordId=None)
-    assert monad.error_status == {"status": 422, "reason": "[{'loc': ['body', 'landlordId'], 'msg': 'none is not an allowed value', 'type': 'type_error.none.not_allowed'}]"}
+    async with aiohttp.ClientSession() as session:
+        request = Request("https://0b008cd5-f83f-40f0-90e8-3c676e55b481.mock.pstmn.io", "/House-422")
+        request.set_session(session)
+        monad = await repository.post(request, **{
+            "lanvcxvcxdlordId": "1"
+        })
+        assert monad.error_status == {"status": 422, "reason": "[{'loc': ['body', 'landlordId'], 'msg': 'field required', 'type': 'value_error.missing'}]"}
 
